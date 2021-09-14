5ea2350bee2df333b8eb7bf33891f94e499b0f20
Vinícios Barbosa <viniciossilva342325@gmail.com>
2021-09-14 20:11:49 -0300

    Inicio do Projeto

--- src/App.js
+++ src/App.js
@@ -1,54 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import ObsWebSocket from 'obs-websocket-js';
-import ObsControllerButton from './components/ObsControllerButton';
-
-import { useState } from 'react';
 
 function App() {
-
-  const obs = new ObsWebSocket();
-
-  const [ botoes, setBotoes ] = useState(<><button className="botao"/><button className="botao"/><button className="botao"/><button className="botao"/><button className="botao"/></>)
-
-  async function iniciar() {
-    await obs.connect({address: '192.168.0.103:4444', password: 'naoseiasenha'}).then(() => console.log('conectado!'))
-    const { scenes } = await obs.send('GetSceneList')
-    
-    setBotoes(
-      <>
-        <ObsControllerButton obs={obs} category="scenes" sceneName={scenes[0].name}/>
-        <ObsControllerButton obs={obs} category="scenes" sceneName={scenes[1].name}/>
-        <ObsControllerButton obs={obs} category="scenes" sceneName={scenes[2].name}/>
-        <ObsControllerButton obs={obs} category="recording" comand="start" />
-        <ObsControllerButton obs={obs} category="recording" comand="pause" />
-      </>
-    )
-  } 
-  
-  iniciar();
-
-
   return (
     <div className="App">
-      <div className="colum">
-        {botoes}
-      </div>
-      
-      <div className="colum">
-        <ObsControllerButton obs={obs} category="recording" comand="stop" />
-        <ObsControllerButton obs={obs} category="recording" comand="resume" />
-        <button className="botao"/>
-        <button className="botao"/>
-        <button className="botao"/>
-      </div>
-      
-      <div className="colum">
-        <button className="botao"/>
-        <button className="botao"/>
-        <button className="botao"/>
-        <button className="botao"/>
-        <button className="botao"/>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
