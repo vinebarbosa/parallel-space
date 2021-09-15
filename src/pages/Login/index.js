f1eb40c3a320c29b42422a5c6d54e35192fecaf4
Vinícios Barbosa <viniciossilva342325@gmail.com>
2021-09-15 00:58:46 -0300

    Pagina de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,17 +2,40 @@ import React from 'react'
 
 import './styles.css'
 
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import fogueteImg from "./../../assets/img/foguete.png"
+
 function Login() {
     return (
         <>
             <h1 className="titulo">Parallel Space JS</h1>
+            
             <div className="login-container">
                 <h2>Faça seu login</h2>
 
-                <div className="input-container">
-                    <input type="email" placeholder="e-mail"/>
+                <div className="centralizar-container" >
+                    <div className="input-container">
+                        <FiMail className="input-container-img" size={24} color="#757575"/>
+                        <input type="email" placeholder="E-mail"/>
+                    </div>
 
+                    <div className="input-container">
+                        <FiLock className="input-container-img" size={24} color="#757575"/>
+                        <input type="password" placeholder="Senha"/>
+                    </div>
+
+                    <p><a href="/">Esqueci minha senha</a></p>
+
+                    <button type="submit">ENTRAR</button>
                 </div>
+
+                <div className="registre-container">
+                    <p>Não tem uma conta?</p>
+                    <p><a href="/">Registre-se</a></p>
+                </div>
+
+                <img className="foguete" src={fogueteImg} alt="foguete"/>
 
             </div>
         </>
