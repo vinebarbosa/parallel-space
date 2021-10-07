929e5bcbc7c4276137abf3938767b5304d503364
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-07 18:57:23 -0300

    definido padrão de estilo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const routes = require("./routes")
+const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, "localhost", () => {
-  console.log("\n🚀 Servidor iniciado!");
+app.listen(3333, 'localhost', () => {
+  console.log('\n🚀 Servidor iniciado!')
 })
