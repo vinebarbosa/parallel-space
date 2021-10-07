ceca504bd606602b9edc5bdc7c50f077f5c102b4
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-07 16:33:51 -0300

    Iniciado o app mobile

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Pedro corno</Text>
+      <StatusBar backgroundColor="#FFF" style="dark" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 18
+  }
+});
