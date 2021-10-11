6b5ca85eff60492b87b6275336585644b260456a
amos-siilva <amossiilva10@gmail.com>
2021-10-11 17:40:07 -0300

    Adicionadas as rotas dos botões

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const userController = require('./controllers/userController')
+const buttonsController = require('./controllers/buttonsController')
 
 const routes = express.Router()
 
 routes.post('/user', userController.create)
 routes.get('/user', userController.get)
+
+routes.get('/buttons', buttonsController.get)
+routes.put('/button', buttonsController.update)
 
 module.exports = routes
