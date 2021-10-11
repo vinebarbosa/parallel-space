4af604bb2c94ee491702f0c5bab2019779441a9e
amos-siilva <amossiilva10@gmail.com>
2021-10-11 15:26:31 -0300

    Criadas as rotas relacionadas ao usuário

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,51 +1,9 @@
 const express = require('express')
+const userController = require('./controllers/userController')
 
 const routes = express.Router()
 
-routes.post('/user', (request, response) => {
-  response.json({
-    mensagem: 'Usuário cadastrado com sucesso!'
-  })
-})
-
-routes.get('/user', (request, response) => {
-  response.json({
-    user: 'Vinícios',
-    token: 'jd29u2nd88'
-  })
-})
-
-routes.put('/button', (request, response) => {
-  console.log(request.body)
-  response.sendStatus(200)
-})
-
-routes.get('/buttons', (request, response) => {
-  response.json([
-    {
-      id: 0,
-      type: 'obs',
-      category: 'stream',
-      scene: null,
-      command: 'start',
-      path: null,
-      icon: 'https://parallelspacejs.com.br/image/23124'
-    },
-    {
-      id: 1,
-      type: 'system',
-      category: 'open-app',
-      scene: null,
-      command: null,
-      path: 'C:/programFiles/chrome/chrome.exe',
-      icon: 'https://parallelspacejs.com.br/image/23124'
-    }
-  ])
-})
-
-routes.get('/image/:id', (request, response) => {
-  console.log(request.params.id)
-  response.json({ imagem: 'taí a tua imagem' })
-})
+routes.post('/user', userController.create)
+routes.get('/user', userController.get)
 
 module.exports = routes
