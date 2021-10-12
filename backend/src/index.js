a74f5858eb37af230faa03822fb8d1aafdf73399
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-11 23:27:38 -0300

    add módulo de segurança cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 
 const routes = require('./routes')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(
