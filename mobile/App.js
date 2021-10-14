9fdf8f58e91e806e95d7e832f0688dbb0fe4100e
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-13 22:33:13 -0300

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Pedro corno</Text>
-      <StatusBar backgroundColor="#FFF" style="dark" />
-    </View>
-  )
+  return <View />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121214',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#FFF',
-    fontSize: 18
-  }
-})
