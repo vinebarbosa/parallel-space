12b679e5c4e5140f0178255782eb3e697a32062f
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-19 10:04:43 -0300

    Update rotas

---
+++ mobile/src/routes/auth.routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import iosPressets from '../configs/iosPresset'
+
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+const Stack = createStackNavigator()
+
+function AuthRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#121214' }
+      }}
+    >
+      <Stack.Screen name="Login" component={Login} options={iosPressets} />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={iosPressets}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default AuthRoutes
---
+++ mobile/src/routes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import AuthRoutes from './auth.routes'
+
+function Routes() {
+  return <AuthRoutes />
+}
+
+export default Routes
