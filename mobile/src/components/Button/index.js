b65970eba83e9fd627da0e6ab505f39bbd387027
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-19 11:04:33 -0300

    Update Input Component

--- mobile/src/components/Button/index.js
+++ mobile/src/components/Button/index.js
@@ -3,9 +3,9 @@ import { TouchableOpacity, Text } from 'react-native'
 
 import styles from './styles'
 
-function Button({ children }) {
+function Button({ children, onPress }) {
   return (
-    <TouchableOpacity style={styles.button}>
+    <TouchableOpacity onPress={onPress} style={styles.button}>
       <Text style={styles.buttonText}>{children}</Text>
     </TouchableOpacity>
   )
