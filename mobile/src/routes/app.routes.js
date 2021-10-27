66340b49a962356c460070df1160f15c8caabec2
vinebarbosa <viniciossilva342325@gmail.com>
2021-10-27 16:18:34 -0300

    Adicionado o contexto do obs às rotas da aplicação

--- mobile/src/routes/app.routes.js
+++ mobile/src/routes/app.routes.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import iosPressets from '../configs/iosPresset'
+import { ObsProvider } from '../contexts/ObsContext'
 
 import Profile from '../pages/Profile'
+import iosPressets from '../configs/iosPresset'
 
 const Stack = createStackNavigator()
 
 function AppRoutes() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-        headerBackground: '#121214',
-        cardStyle: { backgroundColor: '#121214' }
-      }}
-    >
-      <Stack.Screen name="Profile" component={Profile} options={iosPressets} />
-    </Stack.Navigator>
+    <ObsProvider>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerBackground: '#121214',
+          cardStyle: { backgroundColor: '#121214' }
+        }}
+      >
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={iosPressets}
+        />
+      </Stack.Navigator>
+    </ObsProvider>
   )
 }
 
