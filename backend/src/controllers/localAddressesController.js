ec4ee119c267566957be69144c2081195d09079d
vinebarbosa <viniciossilva342325@gmail.com>
2021-11-20 11:43:41 -0300

    Fix backend bugs

--- backend/src/controllers/localAddressesController.js
+++ backend/src/controllers/localAddressesController.js
@@ -3,23 +3,39 @@ const connection = require('../database/connection')
 module.exports = {
   get: async function (request, response) {
     const id = request.headers.authorization
-    // eslint-disable-next-line camelcase
-    const { local_address } = await connection('local_addresses')
-      .select('*')
-      .where('user_id', id)
-      .first()
+    try {
+      // eslint-disable-next-line camelcase
+      const { local_address } = await connection('local_addresses')
+        .select('*')
+        .where('user_id', id)
+        .first()
 
-    return response.json({ localAddress: local_address })
+      return response.json({ localAddress: local_address })
+    } catch {
+      return response.json({ error: 'Usuário não autorizado' })
+    }
   },
-
-  create: async function (request, response) {
+  create: async (user) => {
     const row = {
-      local_address: request.headers.localaddress,
-      user_id: request.headers.authorization
+      local_address: '',
+      user_id: user.id
     }
 
     await connection('local_addresses').insert(row)
+  },
+  update: async function (request, response) {
+    const row = {
+      local_address: request.query.local_address,
+      user_id: request.headers.authorization
+    }
 
-    return response.sendStatus(201)
+    try {
+      await connection('local_addresses')
+        .where('user_id', row.user_id)
+        .update(row)
+      return response.send()
+    } catch (err) {
+      return response.sendStatus(401)
+    }
   }
 }
