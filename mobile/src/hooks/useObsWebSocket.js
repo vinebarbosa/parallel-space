beb6b2b7bea9c1bf31b96e79559d9c76868bef77
vinebarbosa <viniciossilva342325@gmail.com>
2021-12-07 18:04:21 -0300

    feature: Integração com o OBS

--- mobile/src/hooks/useObsWebSocket.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-
-import { ObsContext } from '../contexts/ObsContext'
-
-export default function useObs() {
-  return useContext(ObsContext)
-}
--- mobile/src/routes/app.routes.js
+++ mobile/src/routes/app.routes.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import { ObsProvider } from '../contexts/ObsContext'
-
 import Profile from '../screens/Main'
 import iosPressets from '../configs/iosPresset'
 
@@ -10,21 +8,15 @@ const Stack = createStackNavigator()
 
 function AppRoutes() {
   return (
-    <ObsProvider>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          headerBackground: '#121214',
-          cardStyle: { backgroundColor: '#121214' }
-        }}
-      >
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={iosPressets}
-        />
-      </Stack.Navigator>
-    </ObsProvider>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        headerBackground: '#121214',
+        cardStyle: { backgroundColor: '#121214' }
+      }}
+    >
+      <Stack.Screen name="Profile" component={Profile} options={iosPressets} />
+    </Stack.Navigator>
   )
 }
 
